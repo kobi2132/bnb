bd5331e53ce3f0f04c0e25de55d58fea902e8754
Adi <blue.vixen@gmail.com>
2021-11-30 22:08:23 +0200

    creating services,stores and scss

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { userStore } from './user.store.js'
+import { orderStore } from './order.store.js'
+import { stayStore } from './stay.store.js'
 
 Vue.use(Vuex)
 
@@ -11,5 +14,8 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    userStore,
+    orderStore,
+    stayStore
   }
 })
