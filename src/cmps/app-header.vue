1df8828a750760c45dfefaaccc6df51972f224a5
Adi <blue.vixen@gmail.com>
2021-12-01 13:11:05 +0200

    0112-1310

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <section class="main-header flex space-between">
+    <div class="logo" @click="goHome">
+      <img class="logo-img" src="~@/assets/images/logo.png" />
+    </div>
+    <div id="nav">
+      <router-link to="/explore">Explore</router-link>
+      <router-link to="/">Become a Host</router-link>
+      <button class="user-menu-btn">
+        <img class="avatar" src="~@/assets/images/avatar1.png" />
+      </button>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    goHome() {
+      this.$router.push("/").catch(() => {});
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
