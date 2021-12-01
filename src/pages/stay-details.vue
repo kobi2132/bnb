a121646398a3ebcddb2b5874763380ccacdcba72
Aner Biton <aner.biton@gmail.com>
2021-12-01 13:07:37 +0200

    112-1307

---
+++ src/pages/stay-details.vue
@@ -0,0 +1,47 @@
+  <!-- <pre>{{ stays }} </pre> -->
+  <!-- change rate to rates avg -->
+
+
+<template>
+<section v-if="stay" class="stay-details app-main">
+    <h1>stay details</h1>
+    <h2>{{ stay.name }}</h2>
+    <h4>⭐{{ stay.reviews[0].rate }}</h4>
+    <h4>{{stay.loc.address}}</h4>
+    <button>❤</button>
+    <button>📤</button>
+    <div>stay gallery</div>
+    
+    <h4>
+      Reviews:
+      <span v-for="(review, idx) in stay.reviews" :key="idx">
+        {{ review }}
+      </span>
+    </h4>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "stayDetails",
+  computed: {
+    stay() {
+      return this.$store.getters.getCurrStay;
+    },
+  },
+  methods: {},
+  watch: {
+    "$route.params.stayId": {
+      handler() {
+        let stayId = this.$route.params.stayId;
+        console.log("Changed to", stayId);
+        this.$store.dispatch({ type: "setCurrStay", stayId });
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/store/stay.store.js
+++ src/store/stay.store.js
@@ -3,24 +3,39 @@ import { stayService } from '../../services/stay.service.js'
 export const stayStore = {
     state: {
         stays: [],
+        currStay: null,
+
 
     },
     getters: {
         staysToShow(state) {
             return state.stays
-        }
+        },
+        getCurrStay(state) {
+            return JSON.parse(JSON.stringify(state.currStay))
+        },
     },
     mutations: {
         setStays(state, { stays }) {
             state.stays = stays
-        }
+        },
+        setCurrStay(state, { stay }) {
+            console.log('currStay', state.currstay)
+            state.currStay = stay
+            console.log('currStay', state.currStay)
+        },
     },
     actions: {
         loadStays({ commit }) {
             stayService.query().then(stays => {
                 commit({ type: 'setStays', stays })
             })
-        }
+        },
+        setCurrStay({ commit, getters }, { stayId }) {
+            return stayService.getById(stayId).then((stay) => {
+                console.log(stay)
+                commit({ type: 'setCurrStay', stay })
+            })
+        },
     }
-
 }
