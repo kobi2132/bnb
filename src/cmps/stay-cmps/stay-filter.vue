2ae7bd908e94e209a59283d978d2419322cc48d8
Adi <blue.vixen@gmail.com>
2021-12-02 11:19:25 +0200

    21221-1119

--- src/cmps/stay-cmps/stay-filter.vue
+++ src/cmps/stay-cmps/stay-filter.vue
@@ -1,9 +1,12 @@
 <template>
   <section class="filter-container flex align-center">
-    <button class="mini-filter flex space-between align-center">
+    <button class="mini-filter flex space-between align-center clickable">
       Start your search <span></span>
     </button>
-    <div class="max-filter"></div>
+    <div class="max-filter">
+      <button class="location-btn"></button>
+      <button class="start-date"></button>
+    </div>
   </section>
 </template>
 
