db42b298a1dd5012e706d7266f394ca91e752f73
Adi <blue.vixen@gmail.com>
2021-12-02 11:34:18 +0200

    layouts and stuff

--- src/cmps/stay-cmps/stay-filter.vue
+++ src/cmps/stay-cmps/stay-filter.vue
@@ -1,17 +1,30 @@
 <template>
   <section class="filter-container flex align-center">
-    <button class="mini-filter flex space-between align-center clickable">
+    <button
+      class="mini-filter flex space-between align-center clickable"
+      v-if="miniFilter"
+    >
       Start your search <span></span>
     </button>
     <div class="max-filter">
-      <button class="location-btn"></button>
-      <button class="start-date"></button>
+      <button class="location-btn" placeholder="Where are you going?">
+        Location
+      </button>
+      <button class="start-date" placeholder="Add dates">Check in</button>
+      <button class="end-date" placeholder="Add dates">Check out</button>
+      <button class="guests" placeholder="Add guests">Guests</button>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      miniFilter: false,
+    };
+  },
+};
 </script>
 
 <style>
