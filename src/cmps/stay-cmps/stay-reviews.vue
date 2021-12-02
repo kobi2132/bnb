6d78ccfbd0ec09ef6a90101d4700085b1ef4cb72
Aner Biton <aner.biton@gmail.com>
2021-12-02 23:19:33 +0200

    0212-23:19

--- src/cmps/stay-cmps/stay-reviews.vue
+++ src/cmps/stay-cmps/stay-reviews.vue
@@ -7,38 +7,56 @@
          {{ reviewsRateAvg }}
          ({{this.reviews.length}} reviews) 
         </h2>
+        <div class="stay-reviews-stats">
+          <!-- <star-rating v-model="rating"></star-rating> -->
+        </div>
           <!-- <template v-for="(review, idx) in reviews">
                 
           </template> -->
       </div>
 
 
-      <div class="stay-reviews-stats"></div>
+      
     </section>
 </template>
 
 <script>
+// import StarRating from 'vue-star-rating'
+
+
 export default {
 name: "stayReviews",
   props: {
     reviews: {
-      type: Object,
+      type: Array,
       default: [],
     },
   },
+//   components: {
+//   StarRating
+// },
   computed: {
     reviewsRateAvg(){
-      var sum = 0
+      // console.log('reviews' , this.reviews);
+      // console.log('reviews', this.reviews);
+      var avgsSum = 0
       this.reviews.forEach(review => {
-        sum +=this.reviewRateAvg(review)
-        console.log(sum)
+        // console.log('review rate', review.rate)
+        const sumRates = (obj) => Object.values(obj).reduce((a, b) => a + b);
+        const currSum = sumRates(review.rate);
+        const currSumAvg = currSum/6
+        avgsSum += currSumAvg
+        // console.log('currsum', currSum, 'avgsum', avgsSum)
       });
+      avgsSum= avgsSum/this.reviews.length
+      // console.log( 'final', avgsSum)
+      return avgsSum.toFixed(1)
     },
 
     reviewRateAvg(review) {
-      console.log(review.rate);
-      const sumValues = (obj) => Object.values(obj).reduce((a, b) => a + b);
-      const sum = sumValues(this.review.rate);
+      console.log(review);
+      // const sumValues = (obj) => Object.values(obj).reduce((a, b) => a + b);
+      // const sum = sumValues(review.rate);
       //  const sum = this.stay.reviews.rate.reduce(
       //   (sum, rate) => sum + rate.score,{});
       console.log("sum", sum);
