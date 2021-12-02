c8ef0f7705fe5b5bbbf77687e9519d7621453d04
Aner Biton <aner.biton@gmail.com>
2021-12-02 18:11:36 +0200

    0212-18:11

---
+++ src/cmps/stay-cmps/stay-features.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <section class="stay-feature-container">
+      <div class="feature-block flex">
+        <div class="feature-logo">
+          <span class="material-icons-outlined"> home </span>
+        </div>
+        <section class="feature-text-area">
+          <div class="feature-header"><h4>Entire home</h4></div>
+          <div class="feature-text">
+            <h5>You’ll have the apartment to yourself.</h5>
+          </div>
+        </section>
+      </div>
+      <div class="feature-block flex">
+        <div class="feature-logo">
+          <span class="material-icons-outlined"> location_on </span>
+        </div>
+        <section class="feature-text-area">
+          <div class="feature-header"><h4>Great location</h4></div>
+          <div class="feature-text">
+            <h5>Recent guests gave the location a 5-star rating.</h5>
+          </div>
+        </section>
+      </div>
+      <div class="feature-block flex">
+        <div class="feature-logo">
+          <span class="material-icons-outlined"> auto_awesome </span>
+        </div>
+        <section class="feature-text-area">
+          <div class="feature-header"><h4>Enhanced Clean</h4></div>
+          <div class="feature-text">
+            <h5>
+              This host has committed to our 5-step enhanced cleaning process.
+            </h5>
+          </div>
+        </section>
+      </div>
+      <div class="feature-block flex">
+        <div class="feature-logo">
+          <span class="material-icons-outlined"> event </span>
+        </div>
+        <section class="feature-text-area">
+          <div class="feature-header">
+            <h4>Free cancellation up to 24 hours before check-in</h4>
+          </div>
+          <div class="feature-text">
+            <h5>feel free to be flexible</h5>
+          </div>
+        </section>
+      </div>
+    </section>
+
+</template>
+
+<script>
+export default {
+name: "stayFeatures"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/cmps/stay-cmps/stay-reviews.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="stay-reviews-container">
+
+      <div class="stay-reviews-header">
+        <h2>
+          <span class="material-icons-outlined"> star </span>
+         {{ reviewsRateAvg }}
+         ({{this.reviews.length}} reviews) 
+        </h2>
+          <!-- <template v-for="(review, idx) in reviews">
+                
+          </template> -->
+      </div>
+
+
+      <div class="stay-reviews-stats"></div>
+    </section>
+</template>
+
+<script>
+export default {
+name: "stayReviews",
+  props: {
+    reviews: {
+      type: Object,
+      default: [],
+    },
+  },
+  computed: {
+    reviewsRateAvg(){
+      var sum = 0
+      this.reviews.forEach(review => {
+        sum +=this.reviewRateAvg(review)
+        console.log(sum)
+      });
+    },
+
+    reviewRateAvg(review) {
+      console.log(review.rate);
+      const sumValues = (obj) => Object.values(obj).reduce((a, b) => a + b);
+      const sum = sumValues(this.review.rate);
+      //  const sum = this.stay.reviews.rate.reduce(
+      //   (sum, rate) => sum + rate.score,{});
+      console.log("sum", sum);
+      return sum / 6;
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
