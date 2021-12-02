37d74bb80c2b72ddd6857fdc94857fe399927b2a
Adi <blue.vixen@gmail.com>
2021-12-02 18:36:01 +0200

    wishlist

--- src/store/user.store.js
+++ src/store/user.store.js
@@ -10,23 +10,37 @@ export const userStore = {
         },
     },
     mutations: {
-        setUser(state, { users }) {
+        setUser(state, { user }) {
+            console.log(user.wishList)
             state.loggedinUser = user
+            console.log(state.loggedinUser)
         },
-        setToggleWishList(state, {stayId}){
-            console.log(stayId);
-            const idx = state.loggedinUser.wishList.findIndex((currStay) => currStay === stayId)
-            if (!idx){
-                state.loggedinUser.wishList.push(stayId) 
-            }
-            else state.loggedinUser.wishList.splice(idx, 1)
-        }
+        // setToggleWishList(state, { stayId }) {
+        //     console.log(stayId);
+        //     const idx = state.loggedinUser.wishList.findIndex((currStay) => currStay === stayId)
+        //     if (!idx) {
+        //         state.loggedinUser.wishList.push(stayId)
+        //     }
+        //     else state.loggedinUser.wishList.splice(idx, 1)
+        // }
     },
     actions: {
 
-        toggleWishList({ commit }, { stayId }) {
-            return userService.
-            commit({type: 'setToggleWishList', stayId})
+        async toggleWishList({ commit }, { stayId }) {
+            try {
+                console.log(stayId)
+                const user = await userService.getLoggedinUser()
+                const idx = user.wishList.findIndex(wish => wish === stayId)
+                if (idx === -1) user.wishList.push(stayId)
+                else user.wishList.splice(idx, 1)
+                userService.update(user)
+                commit({ type: 'setUser', user })
+
+            } catch (err) {
+                console.log('Error in load user (user.store):', err);
+                throw err;
+            }
+
         }
     }
 
