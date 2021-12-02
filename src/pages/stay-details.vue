fdd68c945dc99a1a2f2abae898d1f91fc1e746a5
Adi <blue.vixen@gmail.com>
2021-12-02 11:00:42 +0200

    fixed layouts

--- src/pages/stay-details.vue
+++ src/pages/stay-details.vue
@@ -3,51 +3,61 @@
 
 
 <template>
-<section v-if="stay" class="stay-details app-main">
+  <section v-if="stay" class="stay-details app-main main-layout">
     <h2>{{ stay.name }}</h2>
     <section class="short-info flex space-between align-center">
-        <section class="flex">
+      <section class="flex">
         <h4 class="spacer">⭐ {{ this.reviewsAvg }}</h4>
-        <h4>{{this.reviewsCount}}</h4>
+        <h4>{{ this.reviewsCount }}</h4>
         <span class="spacer">•</span>
-        <h4>{{stay.loc.address}}</h4>
-        </section>
-        <section class="flex">
+        <h4>{{ stay.loc.address }}</h4>
+      </section>
+      <section class="flex">
         <button>📤 share</button>
         <button>❤ save</button>
-        </section>
+      </section>
     </section>
 
     <section class="stay-gallery">
-                <img  v-for="(img, idx) in stay.imgUrls" :key="idx" class="img-container clean-list"  :src="img" alt="">
+      <img
+        v-for="(img, idx) in stay.imgUrls"
+        :key="idx"
+        class="img-container clean-list"
+        :src="img"
+        alt=""
+      />
     </section>
 
     <section>
-        <h4>{{stay.summery}}</h4>
-        <h5>{{stay.capacity}} guests | bedrooms | beds | bath </h5>
-        <img class="host-img" :src="stay.host.imgUrl" />
+      <h4>{{ stay.summery }}</h4>
+      <h5>{{ stay.capacity }} guests | bedrooms | beds | bath</h5>
+      <img class="host-img" :src="stay.host.imgUrl" />
     </section>
     <section class="stay-feature-container">
-        <div class="feature-block">
-            <div class="feature-logo"></div>
-            <div class="feature-text"></div>
-        </div>
-        <h4 class="stay-feature-home"> features with icon and details</h4>
-        <h4>features with icon and details</h4>
-        <h4>features with icon and details</h4>
-        <h4>features with icon and details</h4>
+      <div class="feature-block">
+        <div class="feature-logo"></div>
+        <div class="feature-text"></div>
+      </div>
+      <h4 class="stay-feature-home">features with icon and details</h4>
+      <h4>features with icon and details</h4>
+      <h4>features with icon and details</h4>
+      <h4>features with icon and details</h4>
     </section>
     <section class="stay-description">
-        <h4>description</h4>
-        <h3>{{stay.description}}</h3>
+      <h4>description</h4>
+      <h3>{{ stay.description }}</h3>
     </section>
     <section class="stay-amenities">
-        <h4>amenities</h4>
-        <ul class="amenities-list">
-            <li v-for="(amenity, idx) in stay.amenities" :key="idx" class="amenity-container clean-list" >
-                <h5>{{amenity}}</h5>
-            </li>
-        </ul>
+      <h4>amenities</h4>
+      <ul class="amenities-list">
+        <li
+          v-for="(amenity, idx) in stay.amenities"
+          :key="idx"
+          class="amenity-container clean-list"
+        >
+          <h5>{{ amenity }}</h5>
+        </li>
+      </ul>
     </section>
     <h4>
       Reviews:
@@ -74,15 +84,15 @@ export default {
       avg = (Math.round(avg * 100) / 100).toFixed(1);
       return avg;
     },
-    reviewsCount(){
-        let count = this.stay.reviews.length
-        if(count = 1){
-            var str = `(${count} review)` 
-        } else {
-            var str = `(${count} reviews)`
-        }
-        return str
-    }
+    reviewsCount() {
+      let count = this.stay.reviews.length;
+      if ((count = 1)) {
+        var str = `(${count} review)`;
+      } else {
+        var str = `(${count} reviews)`;
+      }
+      return str;
+    },
   },
   methods: {},
   watch: {
@@ -99,53 +109,51 @@ export default {
 </script>
 
 <style>
-.host-img{
-    height: 100px;
-    width: 100px;
-    border-radius: 50%;
-    }
+.host-img {
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+}
 
-.stay-gallery{
-    display: grid;
-    grid-template-columns: repeat(4,minmax(180px,1fr));
-    grid-template-rows: 180px 180px;
-    grid-gap: 8px;
-    gap: 8px;
-    
-    /* padding: none; */
-    /* background-color: lightblue; */
-    /* border-radius: 100px; */
+.stay-gallery {
+  display: grid;
+  grid-template-columns: repeat(4, minmax(180px, 1fr));
+  grid-template-rows: 180px 180px;
+  grid-gap: 8px;
+  gap: 8px;
+
+  /* padding: none; */
+  /* background-color: lightblue; */
+  /* border-radius: 100px; */
 }
 
 .stay-gallery img {
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
 }
 
 .stay-gallery :first-child {
-    grid-column: 1/span 2;
-    grid-row: 1/span 2;
-    border-top-left-radius: 13px; 
-    border-bottom-left-radius: 13px;
+  grid-column: 1 / span 2;
+  grid-row: 1 / span 2;
+  border-top-left-radius: 13px;
+  border-bottom-left-radius: 13px;
 }
 
 .stay-gallery :nth-child(3) {
-    border-top-right-radius: 13px;
+  border-top-right-radius: 13px;
 }
 
 .stay-gallery :nth-child(5) {
-    border-bottom-right-radius: 13px;
+  border-bottom-right-radius: 13px;
 }
 
-.short-info h4{
-    margin-block: 0 ;
-    /* vertical-align: center; */
+.short-info h4 {
+  margin-block: 0;
+  /* vertical-align: center; */
 }
 
 .spacer {
-    margin-inline: 2px;
+  margin-inline: 2px;
 }
-
 </style>
