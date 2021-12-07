c2701b784f4697739ede4c01a097112e09192a5d
Aner Biton <aner.biton@gmail.com>
2021-12-07 11:48:56 +0200

    07.12-11:48

--- src/pages/user-notifications.vue
+++ src/pages/user-notifications.vue
@@ -1,15 +1,27 @@
 <template>
-  <section class="main-layout2">
-      <h1>user-notifications</h1>
+  <section class="notifications-page main-layout2">
+        <h1 class="notifications-header">My notifications</h1>
+      <section class="notifications-list flex column">
+        <div class="notifications-card flex space-between gray-box-shadow align-center">
+          <img src="https://randomuser.me/api/portraits/women/68.jpg" alt="avatar"/>
+          <span class="flex column">
+            <h3>Mor Levi</h3>
+            <h4>03 / 12 / 2021</h4></span>
+          <h4 class="notifications-card-txt">Your order has been sent to Mor Levi</h4>
+          <a class="trips-link" href="#/orders">Read More</a>
+        </div>
+        
+      </section>
+        <button class="clear-all-btn clickable">Clear all</button>
+    
   </section>
 </template>
 
 <script>
 export default {
-name:"user-notifications"
-}
+  name: "user-notifications",
+};
 </script>
 
 <style>
-
 </style>
