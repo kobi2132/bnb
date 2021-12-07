ad9b7bb486413591bce0425dd29b88e857dd07fa
Adi <blue.vixen@gmail.com>
2021-12-07 23:46:07 +0200

    fixed host image

--- src/pages/host.vue
+++ src/pages/host.vue
@@ -1,27 +1,26 @@
 <template>
   <section class="main-layout2 become-host">
-    <img src="~@/assets/images/hero/wp25.jpeg" alt="" class="become-host-img">
+    <img src="~@/assets/images/hero/wp25.jpg" alt="" class="become-host-img" />
     <div class="become-host-info">
-    <h1>become a member of the Mi Casa host family</h1>
+      <h1>become a member of the Mi Casa host family</h1>
       <button class="clickable" @click="goHost">
         <h3>get started</h3>
-        </button>
+      </button>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-name: "host",
+  name: "host",
 
-methods: {
+  methods: {
     goHost() {
       this.$router.push("/dashboard").catch(() => {});
     },
-}
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
