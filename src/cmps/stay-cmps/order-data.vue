684e93595680c8b5edf30a08d450d6047baa274b
Kobi Yamin <kobi2132@gmail.com>
2021-12-12 00:27:07 +0200

    1212 0026

--- src/cmps/stay-cmps/order-data.vue
+++ src/cmps/stay-cmps/order-data.vue
@@ -35,12 +35,18 @@ export default {
   computed: {
     startDateToShow() {
       const currDate = new Date(this.order.dates.start);
-      const dateToDisplay = currDate.getDate() + "/" + currDate.getMonth() + 1 + "/" + currDate.getFullYear();
+      const year = currDate.getFullYear();
+      const month = currDate.getMonth() + 1;
+      const day = currDate.getDate();
+      const dateToDisplay = day + "/" + month + 1 + "/" + currDate.getFullYear();
       return dateToDisplay;
     },
     endDateToShow() {
       const currDate = new Date(this.order.dates.end);
-      const dateToDisplay = currDate.getDate() + "/" + currDate.getMonth() + 1 + "/" + currDate.getFullYear();
+      const year = currDate.getFullYear();
+      const month = currDate.getMonth() + 1;
+      const day = currDate.getDate();
+      const dateToDisplay = day + "/" + month + 1 + "/" + year;
       return dateToDisplay;
     },
   },
